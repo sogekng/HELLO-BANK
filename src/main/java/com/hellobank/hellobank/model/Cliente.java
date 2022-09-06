751de8a67e19eb274024467e94a509b9c51fb8fd
Paulo Henrique <paulohenrique3140@outlook.com>
2022-09-06 18:14:27 -0300

    Add implementacoes da classe cliente

--- src/main/java/com/hellobank/hellobank/model/Cliente.java
+++ src/main/java/com/hellobank/hellobank/model/Cliente.java
@@ -1,5 +1,83 @@
 package com.hellobank.hellobank.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cliente")
 public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Integer id_cliente;
+
+    @Column(name = "nome", length = 70, nullable = false)
+    private String nome;
+
+    @Column(name = "cpf", length = 20, nullable = false)
+    private String cpf;
+
+    @Column(name = "email", length = 70, nullable = false)
+    private String email;
+
+    @Column(name = "senha", length = 70, nullable = false)
+    private String senha;
+
+    @Column(name = "telefone", length = 20, nullable = false)
+    private String telefone;
+
+    public Integer getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(Integer id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
     
 }
