deb23697903ead6ea7ea053ffe1b550a0fa21f47
sogekng <gutsantos3@gmail.com>
2022-09-08 15:40:02 -0300

    update

--- src/main/java/com/hellobank/hellobank/dao/TransacaoDAO.java
+++ src/main/java/com/hellobank/hellobank/dao/TransacaoDAO.java
@@ -1,12 +1,10 @@
 package com.hellobank.hellobank.dao;
 
+import java.util.ArrayList;
 
 import org.springframework.data.repository.CrudRepository;
-
 import com.hellobank.hellobank.model.Transacao;
 
-
-
 public interface TransacaoDAO extends CrudRepository<Transacao, Integer> {
-    
+    public ArrayList<Transacao> findByTipoContaining(String palavraChave);
 }
