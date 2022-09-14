d6a84643c6a043e6e670eee8dd831866c174250d
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-09-14 14:04:58 -0300

    Add EnableWebMvc & ComponentScan annotation

--- src/main/java/com/hellobank/hellobank/HellobankApplication.java
+++ src/main/java/com/hellobank/hellobank/HellobankApplication.java
@@ -2,8 +2,12 @@ package com.hellobank.hellobank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@ComponentScan({"com.hellobank.hellobank"})
 public class HellobankApplication {
 
 	public static void main(String[] args) {
