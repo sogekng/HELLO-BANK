c72b7039fc57d042840e5e8a1d2ae9cc5305abc6
sogekng <gutsantos3@gmail.com>
2022-09-21 17:14:12 -0300

    teste

--- src/main/java/com/hellobank/hellobank/controller/HomeController.java
+++ src/main/java/com/hellobank/hellobank/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.hellobank.hellobank.controller;
 
-
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import com.hellobank.hellobank.model.Conta;
+import com.hellobank.hellobank.services.IContaService;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,12 +14,12 @@ public class HomeController {
 
     @GetMapping("/")
     public String index(){
-        return "/home/index";
+        return "/home/home";
     }
 
     @GetMapping("/home")
     public String home(){
-        return "home/index";
+        return "home/home";
     }
 
     @GetMapping("/sobre")
