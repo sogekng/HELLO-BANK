dd05a547444a81d4e969822f8af2477d720c6d6d
sogekng <gutsantos3@gmail.com>
2022-09-21 17:35:27 -0300

    update

--- src/main/java/com/hellobank/hellobank/controller/LoginController.java
+++ src/main/java/com/hellobank/hellobank/controller/LoginController.java
@@ -34,26 +34,24 @@ public class LoginController {
 
     @PostMapping("/logon")
     public String logon(Model model1, Model model2, Administrador administrador, Cliente cliente, String remember, HttpServletResponse response) throws IOException{
+        Cliente clien = this.serviceCliente.toExistLogin(cliente.getCpf(), cliente.getSenha());
+        Administrador admin = this.serviceAdmin.toExistLogin(administrador.getCpf(), administrador.getSenha());
+        
+        if(admin != null){
+            Integer time = remember != null ? 60*60 : 60*60*24;
+            CookieService.setCookie(response, "nome_admin", admin.getNome(), time);
+            model1.addAttribute("admin", serviceAdmin.listarTodos());
+            model2.addAttribute("clien", serviceCliente.listarTodos());
+            return "redirect:/administradores/home";
 
-        return "redirect:/clientes/home";
-        //Cliente clien = this.serviceCliente.toExistLogin(cliente.getCpf(), cliente.getSenha());
-        //Administrador admin = this.serviceAdmin.toExistLogin(administrador.getCpf(), administrador.getSenha());
-        //
-        //if(admin != null){
-        //    Integer time = remember != null ? 60*60 : 60*60*24;
-        //    CookieService.setCookie(response, "nome_admin", admin.getNome(), time);
-        //    model1.addAttribute("admin", serviceAdmin.listarTodos());
-        //    model2.addAttribute("clien", serviceCliente.listarTodos());
-        //    return "redirect:/administradores/home";
-//
-        //}else if (clien != null) {
-        //    Integer time = remember != null ? 60*60 : 60*60*24;
-        //    CookieService.setCookie(response, "id_cliente", String.valueOf(clien.getId_cliente()), time);
-        //    return "redirect:/clientes/home";
-        //}
-//
-        //model1.addAttribute("error", "Usuario ou senha incorretas");
-        //return "login/login";
+        }else if (clien != null) {
+            Integer time = remember != null ? 60*60 : 60*60*24;
+            CookieService.setCookie(response, "id_cliente", String.valueOf(clien.getId_cliente()), time);
+            return "redirect:/clientes/home";
+        }
+
+        model1.addAttribute("error", "Usuario ou senha incorretas");
+        return "login/login";
     }
 
     @GetMapping("/logout")
