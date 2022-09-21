c3a85ed370f7f9086c8f74ae8a517cc466d56a59
sogekng <gutsantos3@gmail.com>
2022-09-21 13:21:26 -0300

    update

--- src/main/java/com/hellobank/hellobank/controller/OthersController.java
+++ src/main/java/com/hellobank/hellobank/controller/OthersController.java
@@ -1,16 +1,20 @@
-package com.hellobank.hellobank.controlle
+package com.hellobank.hellobank.controller;
+
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
-public class OthersController implements ErrorController
+public class OthersController implements ErrorController{
+
     @RequestMapping("/error")
     public ModelAndView handleError(HttpServletResponse response)
     {
-        ModelAndView modelAndView = new ModelAndView()
+        ModelAndView modelAndView = new ModelAndView();
+
         if (response.getStatus() == HttpStatus.NOT_FOUND.value()) {
             modelAndView.setViewName("error-404");
         }
@@ -22,7 +26,7 @@ public class OthersController implements ErrorController
         }
         else {
             modelAndView.setViewName("error");
-        
+        }
         return modelAndView;
     }
 }
