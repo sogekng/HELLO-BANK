115c10d2f63a92738684dcc19917665b5c3efa5d
sogekng <gutsantos3@gmail.com>
2022-09-21 20:08:52 -0300

    teste

--- src/main/java/com/hellobank/hellobank/controller/LoginController.java
+++ src/main/java/com/hellobank/hellobank/controller/LoginController.java
@@ -44,7 +44,7 @@ public class LoginController {
 
         }else if (clien != null) {
             //model.addAttribute("cliennt", clien);
-            return "redirect:/clientes/home";
+            return "clientes/home";
         }
 
         model.addAttribute("error", "Usuario ou senha incorretas");
