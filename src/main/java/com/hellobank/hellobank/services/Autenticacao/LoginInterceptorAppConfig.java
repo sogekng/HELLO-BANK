502c5ea8ae3a1fc7d2dfba9ec8cdd4e420904f59
sogekng <gutsantos3@gmail.com>
2022-09-21 22:21:43 -0300

    update

--- src/main/java/com/hellobank/hellobank/services/Autenticacao/LoginInterceptorAppConfig.java
+++ src/main/java/com/hellobank/hellobank/services/Autenticacao/LoginInterceptorAppConfig.java
@@ -4,24 +4,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-public class LoginInterceptorAppConfig extends WebMvcConfigurerAdapter {
-
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(new LoginInterceptor())
-        .excludePathPatterns(
-         "/login",
-         "/error",
-         "/logon",
-         "/home",
-         "/sobre",
-         "/register",
-         "/img/**",
-         "/vendor/**",
-         "/js/**",
-         "/favicon.ico",
-         "/css/**"
-        );
-  }
-}
+//@Configuration
+//public class LoginInterceptorAppConfig extends WebMvcConfigurerAdapter {
+//
+//  @Override
+//  public void addInterceptors(InterceptorRegistry registry) {
+//    registry.addInterceptor(new LoginInterceptor())
+//        .excludePathPatterns(
+//         "/login",
+//         "/error",
+//         "/logon",
+//         "/home",
+//         "/sobre",
+//         "/register",
+//         "/img/**",
+//         "/vendor/**",
+//         "/js/**",
+//         "/favicon.ico",
+//         "/css/**"
+//        );
+//  }
+//}
