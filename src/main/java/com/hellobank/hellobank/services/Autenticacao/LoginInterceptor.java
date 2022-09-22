e11b4c8242957d1401dbe3d45af7e4445a84b137
sogekng <gutsantos3@gmail.com>
2022-09-21 21:47:02 -0300

    update

---
+++ src/main/java/com/hellobank/hellobank/services/Autenticacao/LoginInterceptor.java
@@ -0,0 +1,26 @@
+package com.hellobank.hellobank.services.Autenticacao;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import com.hellobank.hellobank.services.CookieService;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+   @Override
+   public boolean preHandle
+      (HttpServletRequest request, HttpServletResponse response, Object handler) 
+      throws Exception {
+      
+      try{
+         if(CookieService.getCookie(request, "id_cliente") != null){
+            return true;
+         }
+      }
+      catch(Exception erro) {}
+      
+      response.sendRedirect("/login");
+      return false;
+   }
+}
---
+++ src/main/java/com/hellobank/hellobank/services/Autenticacao/LoginInterceptorAppConfig.java
@@ -0,0 +1,24 @@
+package com.hellobank.hellobank.services.Autenticacao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class LoginInterceptorAppConfig extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LoginInterceptor())
+        .excludePathPatterns(
+         "/login",
+         "/error",
+         "/logar",
+         "/img/**",
+         "/vendor/**",
+         "/js/**",
+         "/favicon.ico",
+         "/css/**"
+        );
+  }
+}
